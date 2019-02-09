aa7548604d02170a8352c0221391840c54803317
antonellosantos <42184482+antonellosantos@users.noreply.github.com>
2019-02-10 01:13:19 +0800

    AddedFileParserCSV

--- src/designchallenge1/FileParser.java
+++ src/designchallenge1/FileParser.java
@@ -3,5 +3,14 @@ package designchallenge1;
 import java.util.ArrayList;
 
 public abstract class FileParser {
+	
+	protected ArrayList<String[]> linesinFile = new ArrayList<String[]>();	
+	
+	public void parsing() {
+		readData();
+		
+	}
+	
+	public abstract void readData();
 
 }
--- src/designchallenge1/FileParserCSV.java
+++ src/designchallenge1/FileParserCSV.java
@@ -1,18 +1,44 @@
 package designchallenge1;
 
-import java.util.*;
 import java.io.*;
 
 public class FileParserCSV extends FileParser{
 
-	@Override
+	//@Override
 	public void readData() {
+		String filename ="src\\Sample Files\\DLSU Unicalendar.psv";
+		File file = new File(filename);
 		try {
-			File file = new File("src\\Sample Files\\DLSU Unicalendar.psv");
+		
+			FileReader filereader = new FileReader(file);
+			BufferedReader bufferedreader = new BufferedReader(filereader);
+
+			String currentLine = bufferedreader.readLine();
+			
+			while(currentLine != null) { // Reads each line of Unicalendar, stores into an Array and splits with , because its a CSV 
+				
+				String LineinFile[] = currentLine.split(",");
+				linesinFile.add(LineinFile);
+				currentLine = bufferedreader.readLine();
+				
+			}
+			
+			file = new File("src\\Sample Files\\UserEvents.csv");
+			filereader = new FileReader(file);
+			bufferedreader = new BufferedReader(filereader);
+			
+		while(currentLine != null) {// Reads each line of UserEvents, stores into an Array and splits with , because its a CSV
+				
+				String LineinFile[] = currentLine.split(",");
+				linesinFile.add(LineinFile);
+				currentLine = bufferedreader.readLine();
+				
+			}
+			bufferedreader.close();
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
 	}
 
 }
