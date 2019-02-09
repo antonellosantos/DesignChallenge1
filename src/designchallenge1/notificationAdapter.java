229920f5f23832ee33c46bba6744cc1066db6813
antonellosantos <42184482+antonellosantos@users.noreply.github.com>
2019-02-09 21:09:58 +0800

    Files sent on Facebook added and Saved

--- src/designchallenge1/notificationAdapter.java
+++ src/designchallenge1/notificationAdapter.java
@@ -1,5 +1,4 @@
 package designchallenge1;
-import java.awt.Color;
 import java.util.Arrays;
 import sms.SMS;
 import facebook.FBView;
@@ -8,9 +7,6 @@ public class notificationAdapter implements notificationCenter{
 	
 	SMS sms;
 	FBView fb;
-	String eventName;
-	int year,day,month;
-	Color color; 
 	
 	
 	
@@ -20,30 +16,25 @@ public class notificationAdapter implements notificationCenter{
 		fb = messageFacebook;
 		
 	}
-
-	
-	/*
-	public void splitDate() {
-		String date = "2/8/2019";
-		String[] sDate = date.split("/");
-		String year = sDate[2];
-		String month = sDate[1];
-		String day = sDate[0];
-		
-		int dYear = Integer.parseInt(year);
-		int dMonth = Integer.parseInt(month);
-		int dDay = Integer.parseInt(day);
-		
-		System.out.println(dDay + "/" + dMonth + "/" + dYear);
-	}
-	*/
-
+//	public void splitDate() {
+//		String date = "2/8/2019";
+//		String[] sDate = date.split("/");
+//		String year = sDate[2];
+//		String month = sDate[1];
+//		String day = sDate[0];
+//		
+//		int dYear = Integer.parseInt(year);
+//		int dMonth = Integer.parseInt(month);
+//		int dDay = Integer.parseInt(day);
+//		
+//		System.out.println(dDay + "/" + dMonth + "/" + dYear);
+//	}
+//	
 	@Override
 	public void getDate() {
 		
-		
 	}
-	
+
 	@Override
 	public void getTitle() {
 		sms.getEventName();
@@ -56,7 +47,5 @@ public class notificationAdapter implements notificationCenter{
 		sms.getColor();
 		
 	}
-
-
 	
 }
