b6dd6f2a4793d6a06adaaa9305b2a6d5c0a164b6
antonellosantos <42184482+antonellosantos@users.noreply.github.com>
2019-02-10 01:35:36 +0800

    Update notificationAdapter.java

--- src/designchallenge1/notificationAdapter.java
+++ src/designchallenge1/notificationAdapter.java
@@ -1,32 +1,19 @@
 package designchallenge1;
 import java.util.Arrays;
-import sms.SMS;
+import sms.SMSView;
 import facebook.FBView;
 
 public class notificationAdapter implements notificationCenter{
 	
-	SMS sms;
+	SMSView sms;
 	FBView fb;
 	
-	public notificationAdapter(SMS textPhone, FBView messageFacebook) {
+	public notificationAdapter(SMSView textPhone, FBView messageFacebook) {
 		sms = textPhone;
 		fb = messageFacebook;
 		
 	}
-//	public void splitDate() {
-//		String date = "2/8/2019";
-//		String[] sDate = date.split("/");
-//		String year = sDate[2];
-//		String month = sDate[1];
-//		String day = sDate[0];
-//		
-//		int dYear = Integer.parseInt(year);
-//		int dMonth = Integer.parseInt(month);
-//		int dDay = Integer.parseInt(day);
-//		
-//		System.out.println(dDay + "/" + dMonth + "/" + dYear);
-//	}
-//	
+
 	@Override
 	public void getDate() {
 		
@@ -34,14 +21,12 @@ public class notificationAdapter implements notificationCenter{
 
 	@Override
 	public void getTitle() {
-		sms.getEventName();
+		
 		
 	}
 
 	@Override
 	public void labelColor() {
-		
-		sms.getColor();
 		
 	}
 	
